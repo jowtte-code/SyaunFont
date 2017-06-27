e5750c3dc4efe3acf7b97ee844b90b25b2cd24cb
koniti <koniti@users.noreply.github.com>
2017-06-27 10:59:41 +0900

    update some glyphs

---
+++ replace_by_subtable.py
@@ -0,0 +1,30 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+# fontforge -script
+import fontforge
+
+from pprint import pprint
+
+# srcf = fontforge.open("ipaexg.ttf")
+srcf = fontforge.open("IPAexGothic.sfd")
+dstf = srcf
+
+srcglyphs = srcf.glyphs()
+for item in srcglyphs:
+    prop = item.getPosSub("'trad' Traditional Forms lookup 19 subtable")
+
+    # skip < U+2E80 ; not kanji area
+    enc = item.encoding
+    if enc < 0x2E80:
+        continue
+
+    if len(prop) != 0:
+        curfname = item.glyphname
+        subfname = prop[0][2]
+        srcf.selection.select(subfname)
+        srcf.copy()
+        dstf.selection.select(curfname)
+        dstf.paste()
+
+dstf.save("mod.sfd")
+
